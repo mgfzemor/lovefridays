2229f2ca4939629aa0d863a2b8e91dccd7ed094b
RafaAudibert - Deepin <rafaeelaudibert@gmail.com>
2019-10-23 11:52:48 -0300

    :sparkles::recycle: Major layout overhaul

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,15 +3,26 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-require("@rails/ujs").start()
-require("turbolinks").start()
-require("@rails/activestorage").start()
-require("channels")
+require("@rails/ujs").start();
+require("turbolinks").start();
+require("@rails/activestorage").start();
+require("channels");
 
+// Require custom JS
+var $ = require("jquery");
+require("popper");
+require("bootstrap");
+require("perfect-scrollbar");
+require("bootstrap-notify");
+require("dashboard");
+
+// Configure global jQuery
+window.jQuery = $;
+window.$ = $;
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
 //
-// const images = require.context('../images', true)
-// const imagePath = (name) => images(name, true)
+const images = require.context("../../assets/images", true);
+const imagePath = name => images(name, true);
