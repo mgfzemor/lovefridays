06b84f8f89612ca37ad03b5b092a6d049242349f
Mario Figueiro <mario.ufrgs.inf@gmail.com>
2019-10-23 01:56:04 -0300

    :sparkles: introduces seeds for courses and teachers.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+def seed_courses
+  Course.new(code: 'INF01154', name: 'REDES DE COMPUTADORES N').save!
+  Course.new(code: 'INF01151', name: 'SISTEMAS OPERACIONAIS II N').save!
+  Course.new(code: 'INF01147', name: 'COMPILADORES').save!
+  Course.new(code: 'INF01142', name: 'SISTEMAS OPERACIONAIS I N').save!
+end
+
+def seed_teachers
+  Teacher.new(full_name: 'Edson Prestes e Silva Junior', email: 'prestes@inf.ufrgs.br').save!
+  Teacher.new(full_name: 'Bruno Castro da Silva', email: 'bruno.silva@inf.ufrgs.br').save!
+  Teacher.new(full_name: 'Viviane Pereira Moreira', email: 'viviane@inf.ufrgs.br').save!
+  Teacher.new(full_name: 'Luciana Salete Buriol', email: 'buriol@inf.ufrgs.br').save!
+end
+
+seed_courses()
+seed_teachers()
