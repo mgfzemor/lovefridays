139153c9e6ecf20b6260c0dbb5f56f93aa6406f0
RafaAudibert - Deepin <rafaeelaudibert@gmail.com>
2019-10-25 11:07:55 -0300

    :sparkles: Added TeacherCourse semester enum

--- app/models/teacher_course.rb
+++ app/models/teacher_course.rb
@@ -1,6 +1,9 @@
 class TeacherCourse < ApplicationRecord
   belongs_to :teacher
   belongs_to :course
+
+  enum semester: %i[first_semester second_semester]
+
   validates :code, presence: true
   validates :year, presence: true
   validates :semester, presence: true
